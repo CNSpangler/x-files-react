3a9f99186d7a9452a72b652e864b0c2e9f0d231c
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-29 16:25:15 -0700

    start hooks in Home.jsx

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/Header.jsx';
+import Characters from '../Characters/Characters.jsx';
+import { fetchCharacters } from '../../services/x-files-api.jsx';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-      </>
-    );
-  }
-}
+const Home = () => {
+  useEffect(async () => {
+    const characters = await fetchCharacters();
+  });
+
+  const [characters, setCharacters] = useState({ characters: [] });
+
+  return (
+    <>
+      <Header />
+      <Characters characters={characters} />
+    </>
+  );
+};
+
+export default Home;
