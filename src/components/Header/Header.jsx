9adea0fd01ee6c30ff7d1d2b5e8286b73794b478
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-29 14:53:19 -0700

    add Home Link in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Header = ({ title = 'X-Files Characters' }) => (
-  <h1>{title}</h1>
+  <>
+    <h1>{title}</h1>
+    {location !== '/' && <Link to='/'>Home</Link>}
+  </>
 );
 
 Header.propTypes = {
