97119360c9f1976d487521040542aa8e3aa23d42
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-29 20:52:25 -0700

    snapshot test DetailPage

--- src/containers/Detail/Detail.jsx
+++ src/containers/Detail/Detail.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 import Header from '../../components/Header/Header.jsx';
-import Character from '../../components/Character/Character.jsx';
 import { fetchCharacter } from '../../services/x-files-api.jsx';
+import DetailPage from '../../DetailPage/DetailPage.jsx';
 
 const Detail = () => {
   const [character, setCharacter] = useState({});
@@ -19,7 +19,7 @@ const Detail = () => {
     <>
       <Header />
       <p>{character.name}</p>
-      <Character character={character} />
+      <DetailPage character={character}/>
     </>
   );
 };
