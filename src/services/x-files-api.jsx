bc24175bbb5be759592cbee94661fd34322c1e6b
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-29 19:36:22 -0700

    tried forcing uppercase, still not working

--- src/services/x-files-api.jsx
+++ src/services/x-files-api.jsx
@@ -13,7 +13,7 @@ export const fetchCharacter = (name) => {
   return fetch(`https://xfiles-api.herokuapp.com/api/v1/characters/${name}`)
     .then(res => res.json())
     .then(json => json({
-      name: json[0].name,
+      name: json[0].name.toUpperCase(),
       image: json[0].image,
       occupation: json[0].occupation,
       description: json[0].description
